f1015124e08eb968e4da1985fa38ad20b7540b80
Ole Mathias Heggem <olemathias.aa.heggem@gmail.com>
2021-12-05 14:46:21 +0100

    Update sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,7 @@
 <section class="right-content">
     <div class="about">
         <h2>About</h2>
-        <p>TG - Technical Blog is the unofficial rambling place of the Systemstøtte and Tech crews
-            from <a href="http://gathering.org">The Gathering</a>. </p>
+        <p>TG - Technical Blog is the unofficial rambling place for <a href="https://gathering.org">The Gathering</a>. </p>
     </div>
     <div class="crew-list">
         <h2>Filter posts by crew</h2>
@@ -11,8 +10,9 @@
     <div class="data-box">
         <h2>Related sites</h2>
         <ul>
-            <li><a href="http://gathering.org/">The Gathering</a></li>
-            <li><a href="http://kandu.no/">KANDU</a></li>
+            <li><a href="https://gathering.org/">The Gathering</a></li>
+            <li><a href="https://github.com/gathering">The Gathering GitHub</a></li>
+            <li><a href="https://kandu.no/">KANDU</a></li>
         </ul>
     </div>
 
@@ -22,12 +22,12 @@
         </header>
 
         <div class="collaborators-list clearfix">
-            <a href="http://www.nlogic.no/"><img src="<?php echo get_template_directory_uri(); ?>/static/img/partners/nlogic.svg" alt=""></a>
-            <a href="http://www.juniper.net/"><img src="<?php echo get_template_directory_uri(); ?>/static/img/partners/juniper.svg" alt=""></a>
-            <a href="http://www.nextron.no/"><img src="<?php echo get_template_directory_uri(); ?>/static/img/partners/nextron.svg" alt=""></a>
-            <a href="http://www.fortinet.com/"><img src="<?php echo get_template_directory_uri(); ?>/static/img/partners/fortinet.svg" alt=""></a>
-            <a href="http://www.telenor.com/"><img src="<?php echo get_template_directory_uri(); ?>/static/img/partners/telenor.svg" alt=""></a>
-            <a href="http://www.nexthop.no/"><img src="<?php echo get_template_directory_uri(); ?>/static/img/partners/nexthop.svg" alt=""></a>
+            <a href="https://www.nlogic.no/"><img src="<?php echo get_template_directory_uri(); ?>/static/img/partners/nlogic.svg" alt="nlogic logo"></a>
+            <a href="https://www.juniper.net/"><img src="<?php echo get_template_directory_uri(); ?>/static/img/partners/juniper.svg" alt="juniper logo"></a>
+            <a href="https://www.nextron.no/"><img src="<?php echo get_template_directory_uri(); ?>/static/img/partners/nextron.svg" alt="nextron logo"></a>
+            <a href="https://www.fortinet.com/"><img src="<?php echo get_template_directory_uri(); ?>/static/img/partners/fortinet.svg" alt="fortinet logo"></a>
+            <a href="https://www.telenor.com/"><img src="<?php echo get_template_directory_uri(); ?>/static/img/partners/telenor.svg" alt="telenor logo"></a>
+            <a href="https://www.nexthop.no/"><img src="<?php echo get_template_directory_uri(); ?>/static/img/partners/nexthop.svg" alt="nexthop logo"></a>
         </div>
 
     </div>
